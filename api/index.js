ae746b3656721fd7b48466ba0d981813c148b1bf
Lily <zonlily@outlook.com>
2026-09-17 12:22:03 +0800

    feat: route Vercel through Today P1 handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { handleHttpRequest } from "../server.mjs";
+import { handleHttpRequest } from "../p1-server.mjs";
 
 export default async function handler(req, res) {
   return handleHttpRequest(req, res);
